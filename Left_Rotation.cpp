ec5ba9dda8d1e55375ac977caf0a30c64103aba7
Rahulwadhwa157 <rahulwadhwa157@gmail.com>
2020-12-08 18:17:42 +0530

    Solution for Left roatation problem in arrays

---
+++ Left_Rotation.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int>rotated(vector<int>v,int d)
+{
+    int N=v.size();
+    vector<int> a(N);
+    for(int i=0;i<N;i++)
+    {
+        a[(i-d+N)%N]=v[i];
+    }
+    return a;
+}
+
+int main()
+{
+    int N,d;
+    cin>>N>>d;
+    vector<int>a(N);
+    for(int i=0;i<N;i++)
+    {
+        cin>>a[i];
+    }
+    vector<int>b=rotated(a,d);
+    
+    for(int i=0;i<N;i++)
+    {
+        cout<<b[i]<<" ";
+    }
+}
